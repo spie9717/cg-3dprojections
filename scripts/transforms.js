087988181f83f61dfe86fcfda27b7a435f27af94
spie9717 <spie9717@stthomas.edu>
2022-04-04 13:22:49 -0500

    finished parallel and perspective mapping functions, havent tested yet

--- scripts/transforms.js
+++ scripts/transforms.js
@@ -4,14 +4,11 @@ function mat4x4Parallel(prp, srp, vup, clip) {
     let translate = new Matrix(4, 4);
     Mat4x4Translate(translate, scene.prp.x, scene.prp.y, scene.prp.z);
     // 2. rotate VRC such that (u,v,n) align with (x,y,z)
-    
-    //need to normalize n and u
-    /*
-    let n = scene.prp - scene.srp;
-    let u = scene.vup * n;
-    let v = n * u;
-    */ 
-
+    let rotate = new Matrix(4, 4);
+    let n = normalize(scene.prp - scene.srp);
+    let u = normalize(scene.vup.cross(n));
+    let v = n.cross(u);
+    Mat4x4Rotate(rotate, u, v, n);
     // 3. shear such that CW is on the z-axis
     let shear = new Matrix(4, 4);
     let dopx = (LEFT) + (RIGHT) / 2;
@@ -31,18 +28,37 @@ function mat4x4Parallel(prp, srp, vup, clip) {
     Mat4x4Scale(scale, sx, sy, sz);
     //...
     let transform = Matrix.multiply(translate, rotate, shear, tpar, scale);
-    // return transform;
+    return transform;
 }
 
 // create a 4x4 matrix to the perspective projection / view matrix
 function mat4x4Perspective(prp, srp, vup, clip) {
     // 1. translate PRP to origin
+    let translate2 = new Matrix(4, 4);
+    Mat4x4Translate(translate2, scene.prp.x, scene.prp.y, scene.prp.z);
     // 2. rotate VRC such that (u,v,n) align with (x,y,z)
+    let rotate2 = new Matrix(4, 4);
+    let n = normalize(scene.prp - scene.srp);
+    let u = normalize(scene.vup.cross(n));
+    let v = n.cross(u);
+    Mat4x4Rotate(rotate2, u, v, n);
     // 3. shear such that CW is on the z-axis
+    let shear2 = new Matrix(4, 4);
+    let dopx = (LEFT) + (RIGHT) / 2;
+    let dopy = (BOTTOM) + (TOP) / 2;
+    let dopz = NEAR;
+    let shx = (dopx * -1) / dopz;
+    let shy = (dopy * -1) / dopz;
+    Mat4x4ShearXY(shear2, shx, shy);
     // 4. scale such that view volume bounds are ([z,-z], [z,-z], [-1,zmin])
+    let scale2 = new Matrix(4, 4);
+    let sx = (2 * NEAR) / ((RIGHT - LEFT) * FAR);
+    let sy = (2 * NEAR) / ((TOP - BOTTOM) * FAR);
+    let sz = (1 / FAR);
+    Mat4x4Scale(scale2, sx, sy, sz);
     // ...
-    // let transform = Matrix.multiply([...]);
-    // return transform;
+    let transform2 = Matrix.multiply(translate2, rotate2, shear2, scale2);
+    return transform2;
 }
 
 // create a 4x4 matrix to project a parallel image on the z=0 plane
@@ -96,12 +112,16 @@ function Mat4x4Scale(mat4x4, sx, sy, sz) {
                      [0, 0, 0, 1]];
 }
 
+function Mat4x4Rotate(mat4x4, u, v, n) {
+    mat4x4.values = [[u[0], u[1], u[2], 0],
+                     [v[0], v[1], v[2], 0],
+                     [n[0], n[1], n[2], 0],
+                     [0, 0, 0, 1]];
+}
+
 // set values of existing 4x4 matrix to the rotate about x-axis matrix
 function Mat4x4RotateX(mat4x4, theta) {
-    mat4x4.values = [[1, 0, 0, 0],
-                     [0, 1, 0, 0],
-                     [0, 0, 1, 0],
-                     [0, 0, 0, 1]];
+    //mat4x4.values = ...;
 }
 
 // set values of existing 4x4 matrix to the rotate about y-axis matrix
